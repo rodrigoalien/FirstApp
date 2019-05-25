ecd324eb38ed3a5c9122e2215eca6ac4d1a97b78
rodrigoalien <rodrigodacunhaalmeida@gmail.com>
2019-05-25 17:44:21 -0300

    Initial commit

---
+++ app/src/main/java/com/aliensoft/firstapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.aliensoft.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto(View view){
+        TextView texto = findViewById(R.id.textoAtual);
+        texto.setText("Texto alterado!");
+    }
+}
